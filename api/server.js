3bf258b8fdda5210eb4df26837b0d4ccdb42058f
Caroline <carolinefallonlucas@gmail.com>
2021-03-04 14:15:44 -0500

    routers added to server js

--- api/server.js
+++ api/server.js
@@ -2,12 +2,16 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
+const authRouter = require('./auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-server.use('/api/users')
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 server.get('/api', (req, res) => {
     res.json({ message: 'API UP AND RUNNING' });
 })
