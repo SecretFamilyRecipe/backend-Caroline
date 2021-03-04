752f2887eeacc92fddf24eff82427a8d52ef5fee
Caroline <carolinefallonlucas@gmail.com>
2021-03-04 11:29:51 -0500

    troubleshoot heroku again

--- api/server.js
+++ api/server.js
@@ -1,24 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
 
+const server = express()
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
 
-const authRouter = require('./auth/auth-router');
-const usersRouter = require('../users/users-router');
-
-const server = express();
-server.use(express.json());
-server.use(helmet());
-
-if (process.env.NODE_ENV === 'development') {
-    const cors = require('cors');
-    server.use(cors());
-    console.log('using cors')
-}
-
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-
-server.get('/api', (req, res) => {
-    res.status(200).json({ api: 'Family Recipes' });
-});
 module.exports = server
