51dcb4d9ff0888e909240443920692063d58735d
Caroline <carolinefallonlucas@gmail.com>
2021-03-04 00:41:25 -0500

    server fixes

--- api/server.js
+++ api/server.js
@@ -2,19 +2,23 @@ const express = require('express');
 const helmet = require('helmet');
 
 
-
-const authRouter = require('../api/auth/auth-router');
+const authRouter = require('./auth/auth-router');
 const usersRouter = require('../users/users-router');
 
 const server = express();
 server.use(express.json());
 server.use(helmet());
 
+if (process.env.NODE_ENV === 'development') {
+    const cors = require('cors');
+    server.use(cors());
+    console.log('using cors')
+}
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
-server.get('/', (req, res) => {
+server.get('/api', (req, res) => {
     res.status(200).json({ api: 'Family Recipes' });
 });
 module.exports = server
--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
 
-const path = require('path')
-const express = require('express')
+// import server here after creation
+const server = require('./api/server.js');
 
-const server = require('./api/server')
+const port = process.env.PORT || 4000;
 
-const port = process.env.PORT
 
-server.use(express.static(path.join(__dirname, 'client/dist')))
+server.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
 
-server.get('*', (req, res) => {
-  // if you want to serve a SPA using Express you totally can!
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
-})
-
-server.listen(port, () => {
-  console.log('listening on ' + port)
-})
